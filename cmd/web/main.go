5741b887a525b49ce27f78471c8e0ce8121dacff
unsubstantiated-Script <jusfulton@gmail.com>
2026-04-01 13:47:33 -0500

    Wireing up models

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"subscription_service/data"
 	"sync"
 	"syscall"
 	"time"
@@ -44,6 +45,7 @@ func main() {
 		InfoLog:  infoLog,
 		ErrorLog: errorLog,
 		Wait:     &wg,
+		Models:   data.New(db),
 	}
 
 	//listen for signals
