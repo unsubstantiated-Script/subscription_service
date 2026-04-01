7657ba570288cfd77ceb19e526ab783015446920
unsubstantiated-Script <jusfulton@gmail.com>
2026-04-01 12:11:48 -0500

    Setting up config and logs

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/alexedwards/scs/redisstore"
@@ -20,16 +21,27 @@ const webPort = "80"
 func main() {
 	//connect to DB
 	db := initDB()
-	db.Ping()
 
 	// create sessions
 	session := initSession()
 
+	//create logs
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	//create channels
 
 	// create waitgroup
+	wg := sync.WaitGroup{}
 
 	// set up the app config
+	app := Config{
+		Session:  session,
+		DB:       db,
+		InfoLog:  infoLog,
+		ErrorLog: errorLog,
+		Wait:     &wg,
+	}
 
 	// setup mail
 
